b06df2ec8c8ee12e2d839e8d10995edacc30da35
Developer <dev@adrainty.com>
2026-02-26 15:40:29 +0800

    fix: 移除不存在的 TrendUpOutlined 图标导入

--- stock-frontend/src/pages/Position.jsx
+++ stock-frontend/src/pages/Position.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Select, Table, Tag, Card } from "antd";
-import { WalletOutlined, CaretUpOutlined, CaretDownOutlined, DashboardOutlined, TrendUpOutlined } from "@ant-design/icons";
+import { WalletOutlined, CaretUpOutlined, CaretDownOutlined, DashboardOutlined } from "@ant-design/icons";
 import axios from "axios";
 
 const { Option } = Select;
